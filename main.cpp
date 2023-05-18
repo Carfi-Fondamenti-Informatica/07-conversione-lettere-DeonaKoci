1259ce8ff4230bc4967bf24d3f9044286e4a00cf
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-18 17:31:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    char c=0;
+    cin>> c;
+    if (funzione(c)== true){
+        char risultato = conversione (c);
+        cout << risultato;
+    }else{
+        char ris[]="error";
+        cout << ris ;
+    }
+    return 0;
 }

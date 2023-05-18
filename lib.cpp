ab6b8695bbae45d6e8c43ab7104d5a6dd8b321bd
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-18 17:30:52 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
 
+#include "lib.h"
+bool funzione (char c){
+    if(c>=65 and c<=90){
+        return true;
+    }else if(c>=97 and c<=122){
+        return true;
+    }else {
+        return false;
+    }
+}
+char conversione( char c){
+    if(funzione(c)== true){
+
+        if(c>=65 and c<=90){
+            char ris=c+32;
+            return ris;
+        }else if(c>=97 and c<=122){
+            char ris=c-32;
+            return ris;
+        }
+    }else{
+        return false;
+    }
+}
